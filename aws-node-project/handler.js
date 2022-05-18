21aa5a0fc733df65cc2894b926be3f49da5d2f25
Cleonildo Jr <cleu.junior@gmail.com>
2022-05-18 00:56:08 -0300

    First commit

--- aws-node-project/handler.js
+++ aws-node-project/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello World Clou',
+        message: 'Hello World Cloud',
         input: event,
       },
       null,
